6f377fccf9418f2c2a7eacbc73d780bae5f23051
Pavel Gordienko <guzmo@yandex.ru>
2024-07-06 03:44:02 +0300

    feat: add husky config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
+function App(props: any) {
   const [count, setCount] = useState(0);
 
   return (
