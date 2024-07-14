bd0dd9e4c20e7bd04e3014b8f9b32c604938f163
Pavel Gordienko <guzmo@yandex.ru>
2024-07-15 01:07:13 +0300

    feat: add NotFound

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Results from '../Results';
+import NotFound from '../NotFound';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Results />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
