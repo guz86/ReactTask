62360f4085a5a3f2bea0ec4e117de4a38962cef9
Pavel Gordienko <guzmo@yandex.ru>
2024-07-15 01:39:34 +0300

    feat: add char detail panel

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchResult.css';
 import { Character, SearchResultProps } from '../../Interfaces';
 import { extractIdFromUrl } from '../../utils/extractIdFromUrl';
+import CharacterDetailsPanel from '../CharacterDetailsPanel';
 
 const SearchResult: React.FC<SearchResultProps> = ({
   loading,
@@ -11,40 +12,53 @@ const SearchResult: React.FC<SearchResultProps> = ({
   searchData,
   items,
 }) => {
+  const [selectedCharacter, setSelectedCharacter] = useState<Character | null>(
+    null
+  );
+
+  const handleCharacterClick = (character: Character) => {
+    setSelectedCharacter(character);
+  };
+
+  const handleClosePanel = () => {
+    setSelectedCharacter(null); // Reset selectedCharacter to close the panel
+  };
+
   if (error) {
     throw new Error('Manual error for test');
   }
 
   return (
-    <div className="result_side">
-      <h2>Search result: {searchTerm}</h2>
-      {/* <h2>Items: {items}</h2> */}
-      {loading && <div className="spinner"></div>}
-      {error && <p className="error-message">{errorMessage}</p>}
-      {searchData && (
-        <ul>
-          {searchData.map((character: Character) => (
-            <li key={extractIdFromUrl(character.url)}>
-              <div className="character-header">
-                <div className="character-id">
-                  ID: {extractIdFromUrl(character.url)}
+    <div className="search-result-container">
+      <div className="result_side">
+        <h2>Search result: {searchTerm}</h2>
+        {loading && <div className="spinner"></div>}
+        {error && <p className="error-message">{errorMessage}</p>}
+        {searchData && (
+          <ul>
+            {searchData.map((character: Character) => (
+              <li key={extractIdFromUrl(character.url)}>
+                <div className="character-header">
+                  <div className="character-id">
+                    {extractIdFromUrl(character.url)}
+                  </div>
+                  <div className="character-name">{character.name}</div>
+                  <button onClick={() => handleCharacterClick(character)}>
+                    Details
+                  </button>
                 </div>
-                <div className="character-name">{character.name}</div>
-              </div>
-              <div className="character-details">
-                <div>Height: {character.height}</div>
-                <div>Mass: {character.mass}</div>
-                <div>Hair Color: {character.hair_color}</div>
-                <div>Skin Color: {character.skin_color}</div>
-                <div>Eye Color: {character.eye_color}</div>
-                <div>Birth Year: {character.birth_year}</div>
-                <div>Gender: {character.gender}</div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        )}
+        <>All: {items}</>
+      </div>
+      {selectedCharacter && (
+        <CharacterDetailsPanel
+          character={selectedCharacter}
+          onClose={handleClosePanel}
+        />
       )}
-      <>{items}</>
     </div>
   );
 };
