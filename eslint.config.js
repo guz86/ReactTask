bcd295940b916796e51c9fe9034a1354eb44a5d4
Pavel Gordienko <guzmo@yandex.ru>
2024-08-17 18:58:09 +0300

    feat: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,53 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintReactRefresh from "eslint-plugin-react-refresh";
+import eslintReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "@typescript-eslint": tseslint.plugin,
+      "react-hooks": eslintReactHooks,
+      "react-refresh": eslintReactRefresh,
+      react: eslintReact,
+      prettier: prettierPlugin,
     },
   },
-)
+  {
+    ignores: [
+      "dist",
+      "vite-env.d.ts",
+      "node_modules",
+      "coverage",
+      "eslint.config.js",
+      "vite.config.ts",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: globals.browser,
+      parserOptions: ["tsconfig.json", "tsconfig.node.json"],
+      parser: tsParser,
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  }
+);
