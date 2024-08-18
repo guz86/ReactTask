3cdf37f1388ec4c7a73375316b952e2abde23b3c
Pavel Gordienko <guzmo@yandex.ru>
2024-08-19 01:35:35 +0300

    feat: add simple react form add yup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Main } from './components/Main/Main';
 import { UncontrolledForm } from './components/Uncontrolled/UncontrolledForm';
-import { ReactHookForm } from './components/ReactHookForm/ReactHookForm';
+import ReactHookForm from './components/ReactHookForm/ReactHookForm';
 import { Header } from './components/Header/Header';
 
 function App() {
